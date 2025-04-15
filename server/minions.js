ed987ad51434eed735cbb55b13ab033a4343bc93
Elrate99 <vadim300012@gmail.com>
2025-04-15 12:21:58 +0300

    add a work function backend

--- server/minions.js
+++ server/minions.js
@@ -49,5 +49,51 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     }
 })
 
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const work = getAllFromDatabase('work').filter(singleWork => {
+        return singleWork.minionId === req.params.minionId;
+    })
+    res.send(work);
+});
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    const workToAdd = req.body;
+    workToAdd.minionId = req.params.minionId;
+    const createdWork = addToDatabase('work', workToAdd);
+    res.status(201).send(createdWork);
+});
+
+minionsRouter.param('workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    if (work) {
+        req.work = work;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if (req.work.minionId !== req.params.minionId || req.body.minionId !== req.params.minionId) {
+        return res.status(400).send();
+    }
+
+    const updatedWork = updateInstanceInDatabase('work', req.body);
+    res.send(updatedWork);
+})
+
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    if(req.work.minionId !== req.params.minionId) {
+        return res.status(400).send();
+    }
+
+    const deleted = deleteFromDatabasebyId('work', req.work.id);
+    if(deleted) {
+        res.status(204).send();
+    } else {
+        res.status(500).send();
+    }
+})
+
 
 module.exports = minionsRouter
